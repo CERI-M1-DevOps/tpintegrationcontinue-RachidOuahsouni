ef5aa8045b655b8b74e68d41f9014edcb550e301
rachidOuahsouni <rachidou286@gmail.com>
2024-11-07 17:51:07 +0100

    Correction du code V7

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -123,6 +123,7 @@ public class ListeSimple {
 
     public void echanger(Noeud r1, Noeud r2) {
         if (r1 != r2) {
+            return ;
 
             Noeud precedentR1;
             Noeud precedentR2;
